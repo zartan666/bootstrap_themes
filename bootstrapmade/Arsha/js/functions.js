0afd660fb947a961bc0df7a62f39f6195af94053
Raymond <musicsmithnz@gmail.com>
2017-07-19 17:48:32 +1200

    tidy

---
+++ bootstrapmade/Arsha/js/functions.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+    
+    //Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+});			
+	
+			
+new WOW().init()
---
+++ bootstrapmade/Arsha/readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this theme!
+
+Theme Name: Arsha
+Theme URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+Author: BootstrapMade
+Author URL: https://bootstrapmade.com
